7d479274fd042f9d67d14f51ec730993764935c4
Radithia <141038766+dith08@users.noreply.github.com>
2024-09-03 09:58:26 +0700

    [rdt] menggubah judul appbar
    menggubah judul appbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Calculator Bangun Datar'),
         backgroundColor: Colors.teal,
         elevation: 4.0,
       ),
